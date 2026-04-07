ea642e32b4f45d508a9af05be1da90632286ae9d
VanZdev <139448115+ZlaIvaDev@users.noreply.github.com>
2026-04-07 22:29:54 +0900

    added few tests

--- jav.js
+++ jav.js
@@ -2,4 +2,50 @@ function capitalize(arr) {
     return arr.at(0).toUpperCase()+arr.slice(1)
 }
 
-module.exports = capitalize;
+
+
+function reversed(str) {
+    let splitString = str.split("");
+    let reverseStr = splitString.reverse();
+    let combineReversed = reverseStr.join("")
+    return combineReversed;
+}
+
+const calculator = {
+
+    add(a,b)  {
+        return a+b;
+    },
+
+    substract(a,b) {
+        return a-b;
+    },
+
+    multiply(a,b) {
+        return a*b;
+    },
+    divide(a,b) {
+        return a/b;
+    },
+
+};
+
+
+
+
+
+module.exports = { capitalize, reversed, calculator };
+
+
+
+
+
+/*
+A reverseString function that takes a string and returns it reversed.
+A calculator object that contains functions for the basic operations:
+add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
+
+
+
+
+*/
--- jav.test.js
+++ jav.test.js
@@ -1,5 +1,16 @@
-const capitalize = require('./jav.js');
+const { capitalize, reversed, calculator } = require('./jav');
 
 test('capitalizes first letter, ex: cao -> Cao ', () =>{
     expect(capitalize('aloha')).toBe('Aloha')
+})
+
+test('reverses string, ex: aloha -> ahola', () => {
+    expect(reversed('winwin')).toBe('niwniw')
+})
+
+test('celculator: add(1,1) -> should equal 2, .multiply(2,2) -> should equal 4 etc', () =>{
+    expect(calculator.add(1,1)).toBe(2)
+    expect(calculator.multiply(2,2)).toBe(4)
+    expect(calculator.substract(4,4)).toBe(0)
+    expect(calculator.divide(6,2)).toBe(3)
 })
